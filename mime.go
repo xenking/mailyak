ce089da39daecbeeb6cc7cf5b5ac734aa4016435
Vladislav Khutorskoy <support@xenking.pro>
2021-05-17 22:51:44 +0300

    Don't write an empty body. Skip empty parts. (#64)
    This still fixes https://github.com/domodwyer/mailyak/issues/61
    by not writing empty multipart/alternative.
    Still, the display in mails is preserved by not
    writing empty html or plain parts.
    
    Fixes https://github.com/domodwyer/mailyak/issues/63

--- mime.go
+++ mime.go
@@ -157,6 +157,11 @@ func (m *Mail) fromHeader() string {
 
 // writeBody writes the text/plain and text/html mime parts.
 func (m *Mail) writeBody(w io.Writer, boundary string) error {
+	if m.plain.Len() == 0 && m.html.Len() == 0 {
+		// No body to write - just skip it
+		return nil
+	}
+
 	alt := multipart.NewWriter(w)
 
 	if err := alt.SetBoundary(boundary); err != nil {
@@ -165,7 +170,7 @@ func (m *Mail) writeBody(w io.Writer, boundary string) error {
 
 	var err error
 	writePart := func(ctype string, data []byte) {
-		if err != nil {
+		if len(data) == 0 || err != nil {
 			return
 		}
 
